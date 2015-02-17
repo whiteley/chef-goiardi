9d61bb9c1d9ad97970fa6ab7f66aa58bf50813ff
Matt Whiteley <mattwhiteley@gmail.com>
2015-02-17 15:24:46 -0800

    Update integration tests

---
+++ test/integration/default/serverspec/localhost/goiardi_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+describe "Goiardi configuration" do
+
+  describe file("/etc/goiardi/goiardi.conf") do
+    it { should be_a_file }
+    its(:content) { should match('ipaddress = "0.0.0.0"') }
+    its(:content) { should match("port = 4545") }
+  end
+
+end
+
+describe "Goiardi installation" do
+
+  describe port(4545) do
+    it { should be_listening }
+  end
+
+  describe service('goiardi') do
+    it { should be_running }
+    # it { should be_monitored_by(:runit) }
+  end
+
+end
