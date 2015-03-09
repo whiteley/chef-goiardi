90d92c1a6204eb91c5e2dac6bc93314adfab0519
Matt Whiteley <mattwhiteley@gmail.com>
2015-03-09 13:12:55 -0700

    Add supported platform to metadata for supermarket

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'mwhiteley@fastly.com'
 license          'Apache 2.0'
 description      'Installs/Configures goiardi'
 long_description 'Installs/Configures goiardi'
-version          '0.1.0'
+version          '0.1.1'
 
 depends          'runit'
+
+supports         'ubuntu', '>= 12.04'
